7476a76ef5b1cfbad6006e4467bddf4bfce727da
wing <androidwing1992@gmail.com>
2018-05-16 02:00:33 +0800

    完成小鸡饿死，小鸡成神等基本功能

--- pet.js
+++ pet.js
@@ -1,12 +1,22 @@
 'use strict'
-
-const PLAY_INTERNAL = 3 * 60 * 1000 ;
-
-const FEED_INTERNAL = 3 * 60 * 1000 ;
-
-const MAX_FEED_TIMES = 3;
-
+//玩耍的间隔
+const PLAY_INTERNAL = 1 * 60 * 1000 ;
+//喂食的间隔
+const FEED_INTERNAL = 1 * 60 * 1000 ;
+//最大喂养次数
+const MAX_FEED_TIMES = 10;
+//饱食度最大值
+const MAX_FEED_VALUE = 100;
+//喂一次增加的饱食度
+const FEED_VALUE = 10;
+//喂一次增加的经验值基数
 const FEED_EXP = 10;
+//心情值增长系数
+const MOOD_GROWTH = 1.3;
+//饱食度下降速度    每x分钟下降一点
+const FEED_VALUE_DOWN_SPEED = 60 * 1000 * 10;
+//成神所需要的经验值
+const MAX_EXP = 100;
 
 var PetContract = function(){
     //user's game data
@@ -26,18 +36,30 @@ var GameData = function(from){
         zero.setSeconds(0);
     var todayZeroTimeMillis = zero.getTime();
 
+    var currentTimeMillis = Date.parse(new Date());
+
     //pet owner
     this.owner = from;
     //pet experience
     this.exp = 0;
     //day feed count
     this.feedCount = 0;
+    //饱食度
+    this.feedValue = 0;
     //last feed time
-    this.lastFeedTimeMillis = todayZeroTimeMillis;
+    this.lastFeedTimeMillis = currentTimeMillis;
     //this is a value between 0 and 1
     this.mood = 0.1;
     //last play time
-    this.lastPlayTimeMillis = todayZeroTimeMillis;
+    this.lastPlayTimeMillis = currentTimeMillis;
+    
+    this.generation = 0;
+
+    //死亡宠物数量
+    this.diedCount = 0;
+
+    //总分数， 每喂食一次加一分，玩耍一次加一分，成神一只加十分，小鸡死亡减十分
+    this.score = 0;
 }
 
 PetContract.prototype = {
@@ -64,14 +86,37 @@ PetContract.prototype = {
         zero.setMinutes(0);
         zero.setSeconds(0);
         var todayZeroTimeMillis = zero.getTime();
+
+
+        var currentTimeMillis = Date.parse(new Date());
+
+        //上次喂养时间距离现在喂养时间降低的饱食度  每分钟降低一点。
+        var feedDValue = (currentTimeMillis -  gameData.lastFeedTimeMillis) / FEED_VALUE_DOWN_SPEED;
         
-        //clear count after 00:00:00
+        var newFeedValue = gameData.feedValue - feedDValue;
+        
+        //饱食度透支100则，则小鸡死亡
+        if(newFeedValue < -100){
+            gameData.exp = 0;
+            gameData.feedValue = 0;
+            gameData.mood = 0.1;
+            gameData.diedCount = gameData.diedCount + 1;
+            gameData.score = gameData.score - 10;
+            this.saveGameData(gameData);
+            return "pet died!";
+        }
+        //更新饱食度
+        gameData.feedValue = newFeedValue;
+
+        //每天0点更新状态：
+
+        //喂养次数清零
         if(gameData.lastFeedTimeMillis < todayZeroTimeMillis){
             gameData.feedCount = 0;
         }
 
+        //心情清零
         if(gameData.lastPlayTimeMillis < todayZeroTimeMillis){
-            gameData.lastPlayTimeMillis = 0;
             gameData.mood = 0.1;
         }
         
@@ -95,13 +140,17 @@ PetContract.prototype = {
             throw new Error("cann't play with pet until " + (gameData.lastPlayTimeMillis+PLAY_INTERNAL));
         }
 
+        gameData.score = gameData.score + 1;
+        gameData.mood = gameData.mood * MOOD_GROWTH;
 
-        gameData.mood = gameData.mood * 1.3;
-
+        //心情值最大为1
+        if(gameData.mood > 1){
+            gameData.mood = 1;
+        }
         gameData.lastPlayTimeMillis = currentTimeMillis;
 
         this.saveGameData(gameData);
-        
+        return "your pet very happy";
     },
 
 
@@ -115,7 +164,7 @@ PetContract.prototype = {
         var currentTimeMillis = Date.parse(new Date());
 
         if(gameData.feedCount >MAX_FEED_TIMES){
-            throw new Error("already feed 3 times!");
+            throw new Error("already feed "+MAX_FEED_TIMES+" times!");
         }
 
         if(currentTimeMillis - gameData.lastFeedTimeMillis < PLAY_INTERNAL){
@@ -123,9 +172,39 @@ PetContract.prototype = {
         }
 
         gameData.lastFeedTimeMillis = currentTimeMillis;
+
+        //喂食成功加一分
+        gameData.score = gameData.score + 1;
+
+        //如果饱食度为透支状态，则在下一次喂食的时候，直接回复为0，以获得正确的饱食度
+        if(gameData.feedValue < 0){
+            gameData.feedValue = 0;
+        }
+
+        //如果饱食度超出上限，则变为最大值
+        if(gameData.feedValue > MAX_FEED_VALUE){
+            gameData.feedDValue = MAX_FEED_VALUE;
+        }
+
+        gameData.feedValue = gameData.feedValue + FEED_VALUE;
         gameData.feedCount = gameData.feedCount + 1;
+
+
+        //增加经验
         gameData.exp = gameData.exp + FEED_EXP * gameData.mood;
+
+        //宠物成神，自动繁殖下一代宠物。
+        if(gameData.exp > MAX_EXP){
+            gameData.generation = gameData.generation + 1;
+            gameData.exp = 0;
+            gameData.feedCount = 0;
+            gameData.mood = 0.1;
+            gameData.feedValue = 0;
+            //成神加十分
+            gameData.score = gameData.score + 10;
+        }
         this.saveGameData(gameData);
+        return "feed success!";
 
     },
 
